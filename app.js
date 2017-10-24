ebd0a21c8f55b206600129b342488a00c3e782ec
michalsal <michal.saletra@loyaltypoint.pl>
2017-10-24 17:06:21 +0200

    paddle collision added

--- app.js
+++ app.js
@@ -3,12 +3,13 @@ let width = canvas.width;
 let height = canvas.height;
 let canvasContext = canvas.getContext('2d');
 let ballX = width/2;
-let ballSpeedX= 5;
+let ballSpeedX= 2;
 let ballY = height/2;
-let ballSpeedY = 5;
+let ballSpeedY = 2;
 const paddleHeight = 100;
 const paddleWidth = 10;
 let paddle1Y = 250;
+let paddle2Y = 250;
 function getMousePos(canvas, evt){
     let rect = canvas.getBoundingClientRect();
     let root = document.documentElement;
@@ -22,6 +23,8 @@ function drawEverything(){
     colorRect(0,0, width, height, 'black');
     //Draw left player
     colorRect(0,paddle1Y, paddleWidth, paddleHeight, 'white');
+    //Draw right player 
+    colorRect(width-paddleWidth,paddle2Y, paddleWidth, paddleHeight, 'white');
     //Draw ball
     drawBall(ballX, ballY, 10, 'white');
 }
@@ -36,13 +39,26 @@ function drawBall(centerX, centerY, radius, drawColor){
     canvasContext.arc(centerX, centerY, radius, 0, Math.PI*2, true);
     canvasContext.fill();
 }
+function ballReset(){
+    ballSpeedX = -ballSpeedX;
+    ballX = canvas.width/2;
+    ballY = canvas.height/2;
+}
 function moveEverything(){
     ballX += ballSpeedX;
     ballY += ballSpeedY;
     if (ballX > width){
-        ballSpeedX = -ballSpeedX;
+        if ( ballY > paddle2Y && ballY < paddle2Y + paddleHeight){
+            ballSpeedX = -ballSpeedX;
+        } else {
+            ballReset();
+        }
     } else if (ballX < 0){
-        ballSpeedX = -ballSpeedX;
+        if (ballY > paddle1Y && ballY < paddle1Y + paddleHeight){
+            ballSpeedX = -ballSpeedX;
+        } else {
+            ballReset();
+        }
     }
 
     if (ballY > height){
@@ -64,7 +80,7 @@ window.onload = function(){
     canvas.addEventListener('mousemove', function(e){
         let mousePos = getMousePos(canvas, e);
         if(mousePos.y > (height - paddleHeight/2)){
-            mousePos.y = height - paddleHeight/2;
+            paddle1Y = height - paddleHeight;
         } else {
             paddle1Y = mousePos.y - (paddleHeight/2);
         }
